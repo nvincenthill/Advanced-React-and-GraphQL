c9a35180fa0e185ecb499a3e82345fb25e3cd089
mypersonalgithubaccount <nvincenthill@gmail.com>
2018-10-21 14:56:50 -0700

    Add example next page and push state routing

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const Home = props => (
+  <div>
+    <Link href="./sell">
+      <a>Sell!</a>
+    </Link>
+    <h1>Hello, world!</h1>
+  </div>
+);
+export default Home;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,6 @@
+const Sell = props => (
+  <div>
+    <h1>Sell, world!</h1>
+  </div>
+);
+export default Sell;
