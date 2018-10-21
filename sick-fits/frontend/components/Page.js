de4436cdf63832a2b32ebfc9c4d870f19a9ef6d2
mypersonalgithubaccount <nvincenthill@gmail.com>
2018-10-21 16:24:34 -0700

    Add meta to Page component

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
+import Meta from "../components/Meta";
 
 class Page extends React.Component {
   render() {
     return (
       <div>
+        <Meta />
         <Header />
         {this.props.children}
       </div>
