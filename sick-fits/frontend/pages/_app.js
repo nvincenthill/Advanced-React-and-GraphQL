612eb58eb03289c956760d0f7aff0a6fe46f8b3b
mypersonalgithubaccount <nvincenthill@gmail.com>
2018-10-27 15:37:43 -0700

    Expose Apollo API to client pages

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,18 +1,35 @@
 import App, { Container } from "next/app";
+import { ApolloProvider } from "react-apollo";
 import Page from "../components/Page";
+import withData from "../lib/withData";
 
 class MyApp extends App {
+  // useful Next.js life-cycle method to get async data before render() is called
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    // crawls page and resolves any async queries
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    // this exposes the query to the user
+    pageProps.query = ctx.query;
+    // returning anything in getInitialProps() exposes it in props before render() is called
+    return { pageProps };
+  }
+
   render() {
-    const { Component } = this.props;
+    const { Component, apollo, pageProps } = this.props;
 
     return (
       <Container>
-        <Page>
-          <Component />
-        </Page>
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
       </Container>
     );
   }
 }
 
-export default MyApp;
+export default withData(MyApp);
