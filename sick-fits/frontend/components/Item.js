c83e811f2c01d6413cabd6eea27173e1524a0dff
mypersonalgithubaccount <nvincenthill@gmail.com>
2018-10-29 20:03:41 -0700

    Create DeleteItem component

--- sick-fits/frontend/components/Item.js
+++ sick-fits/frontend/components/Item.js
@@ -6,6 +6,7 @@ import Title from "./styles/Title";
 import ItemStyles from "./styles/ItemStyles";
 import PriceTag from "./styles/PriceTag";
 import formatMoney from "../lib/formatMoney";
+import DeleteItem from "./DeleteItem";
 
 class Item extends Component {
   render() {
@@ -35,7 +36,7 @@ class Item extends Component {
             <a> Edit</a>
           </Link>
           <button>Add To Cart</button>
-          <button>Delete</button>
+          <DeleteItem>Delete This Item</DeleteItem>
         </div>
       </ItemStyles>
     );
