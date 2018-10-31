3bb0ac6e6d72c54646d85266a6fbf055fbe1d4c9
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-10-31 11:56:32 -0700

    Create Pagination component

---
+++ sick-fits/frontend/components/Pagination.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import PaginationStyles from "./styles/PaginationStyles";
+
+class Pagination extends Component {
+  render() {
+    return (
+      <PaginationStyles>
+        <p>Pagination here</p>
+      </PaginationStyles>
+    );
+  }
+}
+
+export default Pagination;
