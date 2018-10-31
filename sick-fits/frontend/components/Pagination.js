c15b95dc4596568e0acf307ec90d3d8d20fa9770
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-10-31 16:35:52 -0700

    Implement pagination component

--- sick-fits/frontend/components/Pagination.js
+++ sick-fits/frontend/components/Pagination.js
@@ -1,15 +1,41 @@
-import React, { Component } from "react";
+import React from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 
 import PaginationStyles from "./styles/PaginationStyles";
-
-class Pagination extends Component {
-  render() {
-    return (
-      <PaginationStyles>
-        <p>Pagination here</p>
-      </PaginationStyles>
-    );
+import { perPage } from "../config";
+const PAGINATION_QUERY = gql`
+  query PAGINATION_QUERY {
+    itemsConnection {
+      aggregate {
+        count
+      }
+    }
   }
-}
+`;
+
+const Pagination = props => {
+  return (
+    <PaginationStyles>
+      <Query query={PAGINATION_QUERY}>
+        {({ data, loading, error }) => {
+          if (loading) {
+            return "Loading ... ";
+          }
+          if (error) {
+            return "Error";
+          }
+          const count = data.itemsConnection.aggregate.count;
+          const pages = Math.ceil(count / perPage);
+          return (
+            <p>
+              Page {count} of {pages}
+            </p>
+          );
+        }}
+      </Query>
+    </PaginationStyles>
+  );
+};
 
 export default Pagination;
