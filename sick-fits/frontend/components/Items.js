6e7b82c0c16ee680bd341ad66865bcbd2b0109d1
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-10-31 14:09:59 -0700

    Add pagination component to items page

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -4,6 +4,7 @@ import gql from "graphql-tag";
 import styled from "styled-components";
 
 import Item from "./Item";
+import Pagination from "./Pagination";
 const ALL_ITEMS_QUERY = gql`
   query ALL_ITEMS_QUERY {
     items {
@@ -33,6 +34,7 @@ class Items extends Component {
   render() {
     return (
       <Center>
+        <Pagination />
         <Query query={ALL_ITEMS_QUERY}>
           {({ data, error, loading }) => {
             if (loading) {
@@ -50,6 +52,7 @@ class Items extends Component {
             );
           }}
         </Query>
+        <Pagination />
       </Center>
     );
   }
