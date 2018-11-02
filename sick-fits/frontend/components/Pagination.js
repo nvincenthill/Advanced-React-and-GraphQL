b79c3ebb81832bdbe49619028c330ac47564cbed
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-11-02 13:22:53 -0700

    Implement pagination nav bar

--- sick-fits/frontend/components/Pagination.js
+++ sick-fits/frontend/components/Pagination.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
+import Head from "next/head";
+import Link from "next/link";
 
 import PaginationStyles from "./styles/PaginationStyles";
 import { perPage } from "../config";
@@ -25,12 +27,37 @@ const Pagination = props => {
           return "Error";
         }
         const count = data.itemsConnection.aggregate.count;
-        const pages = Math.ceil(count / perPage);
+        const numPages = Math.ceil(count / perPage);
+        const paginationStr = `Page ${props.page} of ${numPages}`;
         return (
           <PaginationStyles>
-            <p>
-              Page {props.page} of {pages}
-            </p>
+            <Head>
+              <title>Zen Skunk | {paginationStr} </title>
+            </Head>
+            <Link
+              prefetch
+              href={{
+                pathname: "items",
+                query: { page: props.page - 1 }
+              }}
+            >
+              <a className="prev" aria-disabled={props.page <= 1}>
+                Prev
+              </a>
+            </Link>
+            <p>{paginationStr}</p>
+            {/* <p>{count} products</p> */}
+            <Link
+              prefetch
+              href={{
+                pathname: "items",
+                query: { page: props.page + 1 }
+              }}
+            >
+              <a className="prev" aria-disabled={props.page >= numPages}>
+                Next
+              </a>
+            </Link>
           </PaginationStyles>
         );
       }}
