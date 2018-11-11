60ca9cc70e90959692d185739c092b697e6177da
mypersonalgithubaccount <nvincenthill@gmail.com>
2018-11-11 14:00:04 -0800

    Add getUser query and resolver

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -13,7 +13,19 @@ const Query = {
 
   items: forwardTo('db'),
   item: forwardTo('db'),
-  itemsConnection: forwardTo('db')
+  itemsConnection: forwardTo('db'),
+  me(parent, args, ctx, info) {
+    // check if current user id
+    if (!ctx.request.userId) {
+      return null;
+    }
+    return ctx.db.query.user(
+      {
+        where: { id: ctx.request.userId }
+      },
+      info
+    );
+  }
 };
 
 module.exports = Query;
