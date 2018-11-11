7bfaaced40a16846e4f801863026271d40e23f4f
mypersonalgithubaccount <nvincenthill@gmail.com>
2018-11-11 14:19:11 -0800

    Implement frontend user auth view

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,26 +1,31 @@
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
+import User from './User';
 
-const Nav = () => {
-  return (
-    <NavStyles>
-      <Link href="./shop">
-        <a>Shop</a>
-      </Link>
-      <Link href="./sell">
-        <a>Sell</a>
-      </Link>
-      <Link href="./signup">
-        <a>Sign Up</a>
-      </Link>
-      <Link href="./orders">
-        <a>Orders</a>
-      </Link>
-      <Link href="./me">
-        <a>My Account</a>
-      </Link>
-    </NavStyles>
-  );
-};
+const Nav = () => (
+  <NavStyles>
+    <User>
+      {({ data: { me } }) => {
+        if (me) return <p>{me.name}</p>;
+        return null;
+      }}
+    </User>
+    <Link href="./shop">
+      <a>Shop</a>
+    </Link>
+    <Link href="./sell">
+      <a>Sell</a>
+    </Link>
+    <Link href="./signup">
+      <a>Sign Up</a>
+    </Link>
+    <Link href="./orders">
+      <a>Orders</a>
+    </Link>
+    <Link href="./me">
+      <a>My Account</a>
+    </Link>
+  </NavStyles>
+);
 
 export default Nav;
--- sick-fits/frontend/components/User.js
+++ sick-fits/frontend/components/User.js
@@ -3,12 +3,14 @@ import gql from 'graphql-tag';
 import PropTypes from 'prop-types';
 
 const CURRENT_USER_QUERY = gql`
+  query {
     me {
-        id
-        email
-        name
-        permissions
+      id
+      email
+      name
+      permissions
     }
+  }
 `;
 
 const User = props => (
@@ -17,7 +19,7 @@ const User = props => (
   </Query>
 );
 
-User.PropTypes = {
+User.propTypes = {
   children: PropTypes.func.isRequired
 };
 
