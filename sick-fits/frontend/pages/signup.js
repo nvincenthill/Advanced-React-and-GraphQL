b6e568658ade6ba67d508ef7255ac9bc7012b75e
mypersonalgithubaccount <nvincenthill@gmail.com>
2018-11-11 12:49:42 -0800

    Style layout with css grid

--- sick-fits/frontend/pages/signup.js
+++ sick-fits/frontend/pages/signup.js
@@ -1,9 +1,18 @@
 import SignUp from "../components/SignUp";
+import styled from "styled-components";
+
+const Columns = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 20px;
+`;
 
 const SignUpPage = props => (
-  <div>
+  <Columns>
     <SignUp />
-  </div>
+    <SignUp />
+    <SignUp />
+  </Columns>
 );
 
 export default SignUpPage;
