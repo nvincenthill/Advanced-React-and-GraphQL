84a99e6ed64f37773926c6c184c46dc5c9912411
mypersonalgithubaccount <nvincenthill@gmail.com>
2018-11-11 13:52:11 -0800

    Implement JWT verification for user Auth

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,12 +1,24 @@
 require('dotenv').config({ path: 'variables.env' });
+const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
 server.express.use(cookieParser());
-// TODO: use express middleware to populate current user
+
+// decode JWT to get userID for each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // add userId to req for future use
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {
