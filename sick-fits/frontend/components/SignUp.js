0314b8584fb56a57c7825e838ecc0f9188409afd
mypersonalgithubaccount <nvincenthill@gmail.com>
2018-11-11 13:37:16 -0800

    Implement SignUp component

--- sick-fits/frontend/components/SignUp.js
+++ sick-fits/frontend/components/SignUp.js
@@ -6,14 +6,79 @@ import Form from './styles/Form';
 import Error from './ErrorMessage';
 
 import { from } from 'apollo-link';
+
+const SIGNUP_MUTATION = gql`
+  mutation SIGNUP_MUTATION($email: String!, $name: String!, $password: String!) {
+    signUp(email: $email, name: $name, password: $password) {
+      id
+      email
+      name
+    }
+  }
+`;
 class SignUp extends Component {
+  state = {
+    name: '',
+    email: '',
+    password: ''
+  };
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
     return (
-      <Form>
-        <fieldset>
-          <h2>Sign Up</h2>
-        </fieldset>
-      </Form>
+      <Mutation mutation={SIGNUP_MUTATION} variables={this.state}>
+        {(signUp, { error, loading }) => {
+          return (
+            <Form
+              method="post"
+              onSubmit={async e => {
+                e.preventDefault();
+                await signUp();
+                this.setState({ name: '', email: '', password: '' });
+              }}
+            >
+              <fieldset disabled={loading} aria-busy={loading}>
+                <h2>Sign Up</h2>
+                <Error error={error} />
+                <label htmlFor="email">
+                  Email
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Enter your email"
+                    value={this.state.email}
+                    onChange={this.handleChange}
+                  />
+                </label>
+                <label htmlFor="name">
+                  Name
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder="Enter your name"
+                    value={this.state.name}
+                    onChange={this.handleChange}
+                  />
+                </label>
+                <label htmlFor="password">
+                  Password
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="Enter your password"
+                    value={this.state.password}
+                    onChange={this.handleChange}
+                  />
+                </label>
+                <button type="submit">Sign Up!</button>
+              </fieldset>
+            </Form>
+          );
+        }}
+      </Mutation>
     );
   }
 }
